aed782afc02f4b79898b8d9a0a699ead50ee23d3
WaDelma <Antti@MahComputer.(none)>
2013-01-26 00:33:16 +0200

    Initial commit of Unity files.

---
+++ Assets/Projectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	
+	public Projectile(Texture[] animation){
+		//this.animation = animation;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
