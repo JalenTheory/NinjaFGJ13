4ecd1b93f0b352775aa009ce6e120af2c2010e5a
chiem <joa0000@hotmail.com>
2013-01-26 17:55:19 +0200

    doesnt work but updated
    contains the platformercontroller from a 2dgametutorial

--- Assets/MC.cs
+++ Assets/MC.cs
@@ -6,12 +6,16 @@ public class MC : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		
+	}
 	
+	void OnControllerHit(ControllerColliderHit c){
+		print (c.gameObject.name);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 	
 	 
