e8723e1d5041bd135debb862ddafe2363f0b7485
WaDelma <Antti@MahComputer.(none)>
2013-01-26 02:08:39 +0200

    Added Projectile.

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -2,18 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
-	
-	public Projectile(Texture[] animation){
-		//this.animation = animation;
-	}
+	public float dir;
+	public float velocity;
 
 	// Use this for initialization
 	void Start () {
-		
+		transform.Rotate(new Vector3(0, dir, 0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(new Vector3(velocity, 0, 0) * Time.deltaTime);
 	}
 }
